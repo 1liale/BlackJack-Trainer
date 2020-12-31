edcbc62bed876ff7b7f4f8201c662c44ae38941e
1liale <1liale@hdsb.ca>
2020-12-30 23:41:38 -0500

    reorganize project

---
+++ src/DealerHand.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ * A dealer's src.Hand, extends src.Hand
+ * Author: Yifan Zong
+ * Created on: 23/12/2020
+ */
+public class DealerHand extends Hand{
+	public DealerHand(Card...cards) {
+		super(cards);
+	}
+}
