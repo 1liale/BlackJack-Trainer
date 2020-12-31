97b654d18b839906f06c061aabd461dd107ee912
1liale <1liale@hdsb.ca>
2020-12-30 22:29:03 -0500

    added comments to fileio

--- BlackJack.java
+++ BlackJack.java
@@ -1,5 +1,6 @@
 /**
  * Console Based Blackjack Game Trainer
+ * Standard Blackjack Rules: H17, DAS, RAS, 3:2 payout
  * Require UTF8 encoding to view properly (see Card for more info)
  * Author: Yifan Zong
  * Created on: 23/12/2020
@@ -10,9 +11,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class BlackJack {
-	private static int sets, reserve;
-	private static double rate;
+public class Blackjack {
+	private static int sets, cutLength;
 	private static Dealer dealer = new Dealer();
 	private static Player[] players;
 	private static Shoe shoe;
@@ -39,29 +39,7 @@ public class BlackJack {
 			}
 		}
 		shoe = new Shoe(sets);
-
-		//Payout rate
-		System.out.println("What is the payout rate?");
-		System.out.println("1. 3:2\n2. 6:5");
-		while (rate == 0.0) {
-			try {
-				input = reader.readLine().trim();
-				switch (input) {
-				case "1":
-					rate = 2.5;
-					break;
-				case "2":
-					rate = 2.2;
-					break;
-				default:
-					System.out.println("Please enter \"1\" for 3:2 and \"2\" for 6:5.");
-					break;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
+		
 		//Number of players
 		System.out.println("How many players?");
 		while (true) {
@@ -82,17 +60,17 @@ public class BlackJack {
 		}
 
 		//Deck penetration
-		int minReserve = 5 * (1 + players.length);
+		int minCutLength = 5 * (players.length) + 9;
 		System.out.println("How deep you want to go down the deck?");
 		System.out.println("Enter as a decimal. e.g. 0.65 for 65%.");
 		while (true) {
 			try {
 				input = reader.readLine();
 				double percentage = Double.parseDouble(input);
-				reserve = (int) (52 * sets * (1 - percentage));
+				cutLength = (int) (52 * sets * (1 - percentage));
 				if (percentage <= 0 || percentage >= 1) {
 					System.out.println("Please enter a number greater than 0 and less than 1.");
-				} else if (reserve < minReserve) {
+				} else if (cutLength < minCutLength) {
 					System.out.println("You may run out of cards mid-round. Please choose a lower deck penetration");
 				} else {break;}
 			} catch (IOException e) {
@@ -110,11 +88,12 @@ public class BlackJack {
 			int type = 0;
 			System.out.println("What kind of player is player" + i + "?");
 			System.out.println("1. Human");
-			System.out.println("2. Basic Strat CPU");
+			System.out.println("2. Basic Strat AI");
+			System.out.println("3. Card Counter AI");
 			while (true) {
 				try {
 					type = Integer.parseInt(reader.readLine());
-					if (type <= 0 || type > 2) {
+					if (type <= 0 || type > 3) {
 						System.out.println("Please enter a valid integer.");
 					} else {
 						break;
@@ -140,6 +119,10 @@ public class BlackJack {
 							break;
 						case 2:
 							players[i] = new BasicStratPlayer(bankroll);
+							break;
+						case 3:
+							players[i] = new CardCountingPlayer(bankroll);
+							break;
 						}
 						break;
 					}
@@ -161,88 +144,182 @@ public class BlackJack {
 	public static void main(String[] args) {
 		//Setup game and players
 		setUp();
+		System.out.println();
 		choosePlayer();
 		System.out.println();
 		
 		//Play game
 		while (true) {
-			while (shoe.length() > reserve) {
-				DealerHand dealerHand = dealer.newHand(); //Dealer draw card
-				
+			while (shoe.length() > cutLength) {
 				//Players place bets
-				int counter = 0; //counter keeps track of the number of players who lost all their bankroll (ruined)
+				int ruined = 0; //counter keeps track of the number of players who lost all their bankroll (ruined)
 				for (int i = 0; i < players.length; i++) {
 					if (!players[i].ruined()) {
 						System.out.println("Player" + i + ":");
-						players[i].newHand();
-						players[i].placeBets();
-						System.out.println();
+						System.out.println("Bankroll: " + players[i].bankroll() + "  Profit: " + players[i].profit());
+						players[i].clearHands().placeBets();
 					} else {
-						counter++;
+						ruined++;
 					}
 				}
+				System.out.println();
 				
-				//If all players lost their bankroll, end the game
-				if (counter == players.length) {
+				//If all players are ruined, end the game
+				if (ruined == players.length) {
 					System.out.println("All players are ruined.");
 					return;
 				}
 				
-				//Players play their hands
+				//Print the dealer's upcard and players' cards
+				dealer.newHand();
+				Card dealerCard = dealer.first();
+				System.out.println("Dealer: [" + dealerCard +"]");
 				for (int i = 0; i < players.length; i++) {
 					if (!players[i].ruined()) {
-						System.out.println("Dealer: " + dealerHand);
-						System.out.print("Player" + i + ": ");
-						players[i].play(dealerHand); //Dealer's one-card hand is passed as parameter
-						System.out.println();
+						players[i].newHand();
+						System.out.println("Player" + i + ": " + players[i].hands().get(0));
 					}
 				}
+				System.out.println();
+				
+				//If dealer has ace as the upcard
+				if (dealerCard.val() == 11) {
+					//Offer players insurance
+					for (int i = 0; i < players.length; i++) {
+						if (!players[i].ruined()) {
+							System.out.println("Player" + i + ":");
+							players[i].placeInsurances();
+						}
+					}
+					System.out.println();
+					
+					//If dealer has blackjack, evaluate win/lose immediately, without the players playing
+					if (dealer.hand().val() == 21) {
+						System.out.println("Dealer: " + dealer.reveal()); //reveal() updates the running count
+						for (int i = 0; i < players.length; i++) {
+							if (! players[i].ruined()) {
+								System.out.println("Player" + i + ":");
+								for (int j = 0; j <  players[i].hands().size(); j++) {
+									ArrayList<PlayerHand> playerHands =  players[i].hands();
+									System.out.print(playerHands.get(j));
+									if (playerHands.get(j).val() == 21) { //If player also has blackjack
+										System.out.println(" Blackjack!");
+										if (playerHands.get(j).insured()) {
+											System.out.println("Even Money");
+											 players[i].update(2.5 * playerHands.get(j).bet());
+										} else {
+											System.out.println("Push");
+											 players[i].update(playerHands.get(j).bet());
+										}
+									} else {
+										if (playerHands.get(j).insured()) { //If player doesn't have blackjack
+											System.out.println(" Insured");
+											players[i].update(1.5 * playerHands.get(j).bet());
+										} else {
+											System.out.println(" Lose");
+											players[i].update(0.0);
+										}
+									}
+								}
+							}
+						}
+						
+						System.out.println();
+						continue;
+					}
+				}
+				
+				//Players play their hands
+				boolean allBusted = true;
+				for (int i = 0; i < players.length; i++) {
+					if (!players[i].ruined()) {
+						System.out.println("Player" + i + ": ");
+						players[i].play(dealerCard); //Dealer's card is passed as parameter
+						
+						//Check if player has busted
+						if (!players[i].busted()) {
+							allBusted = false;
+						} else {
+							players[i].update(0.0);
+						}
+					}
+				}
+				System.out.println();
 
-				//Dealer plays its hand and players win or lose
-				dealerHand = dealer.play();
-				System.out.println("Dealer: " + dealerHand);
+				
+				//Dealer does not play if all players have busted
+				if (allBusted) {
+					System.out.println("All players have busted.");
+					System.out.println("Dealer: " + dealer.reveal());
+					System.out.println();
+					continue;
+				}
+				
+				//Dealer plays their hand
+				boolean dealerBlackjack = false, dealerBusted = false;
+				System.out.print("Dealer: " + dealer.play());
+				//If dealer has blackjack
+				if (dealer.hand().val() == 21 && dealer.hand().size() == 2) {
+					dealerBlackjack = true;
+					System.out.println(" Blackjack!");
+				}
+				//If dealer has busted
+				if (dealer.hand().busted()) {
+					dealerBusted = true;
+					System.out.println(" Bust");
+				} else {
+					System.out.println();
+				}
+				
+				
+				//For each player's hand, compare with the dealer's to determine the winner
 				for (int i = 0; i < players.length; i++) {
 					Player player = players[i];
 					if (!player.ruined()) {
 						System.out.println("Player" + i + ": ");
 						ArrayList<PlayerHand> playerHands = player.hands();
-						
-						//For each player's hand, compare with the dealer's to determine the winner
 						for (int j = 0; j < playerHands.size(); j++) {
-							System.out.println(playerHands.get(j));
-							int comparison = dealerHand.compareTo(playerHands.get(j));
-							switch (comparison) {
-							case 2:
-								System.out.println("BlackJack!");
-								player.update(rate * playerHands.get(j).bet()); 
-								break;
-							case 1:
-								System.out.println("Wins");
-								player.update(2.0 * playerHands.get(j).bet());
-								break;
-							case 0:
-								System.out.println("Push");
+							System.out.print(playerHands.get(j));
+							//Player busts
+							if (playerHands.get(j).busted()) {
+								System.out.println(" Bust"); 
+								//No update since we already done so earlier
+								continue;
+							}
+							//Player has blackjack but dealer does not
+							if (playerHands.get(j).val() == 21 && playerHands.get(j).size() == 2 && !dealerBlackjack) {
+								System.out.println(" Blackjack!");
+								player.update(2.5 * playerHands.get(j).bet());
+								continue;
+							}
+							//Both have blackjack or same value
+							if (playerHands.get(j).val() == dealer.hand().val()) {
+								System.out.println(" Push");
 								player.update(playerHands.get(j).bet());
-								break;
-							case -1:
-								System.out.println("Loses");
-								player.update(0.0); //Require to update whether a player is ruined
-								break;
-							case -2:
-								System.out.println("Busts");
+								continue;
+							}
+							//Dealer busts but player does not or player has a higher non-blackjack hand
+							if (dealerBusted || playerHands.get(j).val() > dealer.hand().val()) {
+								System.out.println(" Win");
+								player.update(2.0 * playerHands.get(j).bet());
+								continue;
+							}
+							//Player does not bust but has a lower value hand
+							if (playerHands.get(j).val() <  dealer.hand().val()) {
+								System.out.println(" Lose");
 								player.update(0.0);
-								break;
+								continue;
 							}
 						}
-						
-						System.out.println();
 					}
 				}
+				System.out.println();
 			}
 			
 			//Reshuffle if end of a shoe is reached
 			shoe.shuffle();
 			System.out.println("Reshuffling");
+			System.out.println();
 		}
 	}
 }
--- FileIO.java
+++ FileIO.java
@@ -1,3 +1,9 @@
+/**
+ * Purpose: Handles file IO (reads and stores chart from txt)
+ * Author: Alex Li
+ * Created on: 23/12/2020
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,6 +13,7 @@ public class FileIO {
     protected int[][] rowValues;
     protected File myFile;
 
+    // initialize basicChart and rowValues
     public FileIO()
     {
         myFile = new File("BasicStrat.txt");
@@ -15,22 +22,26 @@ public class FileIO {
         readFile();
     }
 
+    // returns the basic strat chart
     public char[][][] getChart()
     {
         return basicChart;
     }
 
+    // returns row values of the basic strat chart
     public int[][] getRowValues()
     {
         return rowValues;
     }
 
+    // reads from txt file
     private void readFile()
     {
         try {
             Scanner sc = new Scanner(myFile);
             int chartNum = 0;
             int i = 0, j = 0;
+            // read line if available
             while(sc.hasNextLine())
             {
                 String rawLine= sc.nextLine();
@@ -38,13 +49,16 @@ public class FileIO {
                 int rowVal = Integer.parseInt(splitLines[0]);
                 if(rowVal != -1)
                 {
+                    // store char from BlackJackStrat.txt in basicChart
                     for(j = 0; j < 10; j++)
                     {
                         basicChart[chartNum][i][j] = splitLines[1].charAt(j);
                     }
+                    // store each row's rowVal in rowValues
                     rowValues[chartNum][i] = rowVal;
                     i++;
                 }
+                // separates each chart
                 else {
                     i = 0;
                     chartNum++;
